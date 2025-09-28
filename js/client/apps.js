11ab274b28d838f0023c4fc0239a351c398669be
Dmitriy Kazancev <hevoid47@gmail.com>
2025-09-28 21:38:31 +0500

    deleting apps functionality

--- js/client/apps.js
+++ js/client/apps.js
@@ -1,6 +1,6 @@
 const games = document.querySelectorAll(".games .app");
 
-const gamesBlock = document.querySelector(".games")
+const gamesBlock = document.querySelector(".games");
 
 const websites = document.querySelectorAll(".websites .app");
 const webview = document.getElementById("webview");
@@ -12,16 +12,12 @@ const newwebsiteBtn = document.querySelector(".newwebsiteButton");
 const createApp = document.querySelector(".createApp");
 const createAppBtn = document.querySelector(".createApp button");
 const createAppInput = document.querySelector(".createAppInput");
-const createAppInputFile = document.querySelector(".createAppInputFile");
 
-
-const closeCreateApp = document.querySelector(".createApp svg")
-
+const closeCreateApp = document.querySelector(".createApp svg");
 
 class Apps {
 	constructor() {
 		this.apps = []
-
 		this.restoreApps()
 	}
 
@@ -33,11 +29,9 @@ class Apps {
 		appIconBlock.classList.add("app-icon")
 
 		const imgBlock = document.createElement("img")
-
 		imgBlock.src = iconSrc
 
 		appIconBlock.appendChild(imgBlock)
-
 		appBlock.appendChild(appIconBlock)
 
 		gamesBlock.appendChild(appBlock)
@@ -47,52 +41,57 @@ class Apps {
 		const response = await window.electronAPI.getApps()
 		this.apps = response.apps
 
-		console.log
-
-
 		this.apps.forEach((app) => {
-			console.log(app.icon)
-			this.newgame(app.title, app.icon)
+			this.newgame(app.title, app.icon, app.id, app.execute)
 		})
 	}
 
-	async newgame(title, iconSrc) {
-			const appBlock = document.createElement('div')
-			appBlock.classList.add("app")
+	deleteApp(appBlock) {
+		if (gamesBlock.contains(appBlock)) {
+			gamesBlock.removeChild(appBlock)
+		}
+	}
 
-			const appIconBlock = document.createElement('div')
-			appIconBlock.classList.add("app-icon")
+	async newgame(title, iconSrc, id, executePath) {
+		const appBlock = document.createElement('div')
+		appBlock.classList.add("app")
 
-			appBlock.appendChild(appIconBlock)
+		const appIconBlock = document.createElement('div')
+		appIconBlock.classList.add("app-icon")
 
-			const imgBlock = document.createElement('img')
-			imgBlock.src = iconSrc
+		const imgBlock = document.createElement('img')
+		imgBlock.src = iconSrc
 
-			console.log(iconSrc)
+		const deleteAppBtn = document.createElement('img')
+		deleteAppBtn.classList.add("delete-app")
+		deleteAppBtn.src = import.meta.dirname + "/../../media/close.svg"
 
+		deleteAppBtn.addEventListener('click', async () => {
+			const response = await window.electronAPI.getApps()
 
-			let executeF = ""
+			const updatedApps = response.apps.filter(app => app.id !== id)
+			this.apps = updatedApps
 
-			this.apps.forEach((app) => {
-				if (app.title == title) {
-					executeF = app.execute
-				}
-			})
+			await window.electronAPI.writeApps(updatedApps)
 
+			this.deleteApp(appBlock)
+		})
 
-			appBlock.addEventListener('click', () => {
-				window.electronAPI.openExecute(executeF)
-			})
+		appBlock.addEventListener('click', () => {
+			if (executePath) {
+				window.electronAPI.openExecute(executePath)
+			}
+		})
 
+		appBlock.appendChild(appIconBlock)
+		appBlock.appendChild(imgBlock)
+		appBlock.appendChild(deleteAppBtn)
 
-
-			gamesBlock.appendChild(appBlock)
-			appBlock.appendChild(imgBlock)
+		gamesBlock.appendChild(appBlock)
 	}
 }
 
-const appsObj = new Apps()
-
+const appsObj = new Apps();
 
 createAppBtn.addEventListener("click", async () => {
 	if (createAppInput.value === "") {
@@ -103,75 +102,71 @@ createAppBtn.addEventListener("click", async () => {
 	const res = await window.electronAPI.getDialogPath()
 
 	if (res) {
+		const newApp = {
+			title: createAppInput.value,
+			execute: res.path,
+			icon: res.icon,
+			id: Date.now()
+		}
 
+		appsObj.apps.push(newApp)
 
-			const newApp = {
-				title: createAppInput.value,
-				execute: res.path,
-				icon: res.icon
-			}
+		await window.electronAPI.writeApps(appsObj.apps)
 
-			console.log(appsObj)
-
-			appsObj.apps.push(newApp)
-
-			window.electronAPI.writeApps(appsObj.apps);
-
-			appsObj.newgame(newApp.title, res.icon)
+		appsObj.newgame(newApp.title, newApp.icon, newApp.id, newApp.execute)
 	}
 
-
 	createApp.style.display = "none";
-});
-
-closeCreateApp.addEventListener('click', () => {
-	createApp.style.display = "none"
 })
 
+
+closeCreateApp.addEventListener("click", () => {
+  createApp.style.display = "none";
+});
+
 function showContentApps() {
-	contentApps.classList.add("active");
-	if (appsBlock) appsBlock.style.display = "none";
+  contentApps.classList.add("active");
+  if (appsBlock) appsBlock.style.display = "none";
 }
 
 websites.forEach((website) => {
-	website.addEventListener("click", (e) => {
-		e.preventDefault();
-		try {
-			switch (website.id) {
-				case "youtube":
-				webview.src = "https://www.youtube.com";
-				break;
-				case "twitch":
-				webview.src = "https://www.twitch.tv";
-				break;
-				default:
-				break;
-			}
-			showContentApps();
-		} catch (error) {
-			console.error("website switch error")
-		}
-	});
+  website.addEventListener("click", (e) => {
+    e.preventDefault();
+    try {
+      switch (website.id) {
+        case "youtube":
+          webview.src = "https://www.youtube.com";
+          break;
+        case "twitch":
+          webview.src = "https://www.twitch.tv";
+          break;
+        default:
+          break;
+      }
+      showContentApps();
+    } catch (error) {
+      console.error("website switch error");
+    }
+  });
 });
 
 games.forEach((game) => {
-	game.addEventListener('click', () => {
-		try {
-			switch (game.id) {
-				case "dota2":
-					window.electronAPI.openSteamApp(570)
-					break;
+  game.addEventListener("click", () => {
+    try {
+      switch (game.id) {
+        case "dota2":
+          window.electronAPI.openSteamApp(570);
+          break;
 
-				default:
-					break;
-			}
-		} catch (err) {
-			console.error("error in games switch: " + err)
-		}
-	})
-})
-
+        default:
+          break;
+      }
+    } catch (err) {
+      console.error("error in games switch: " + err);
+    }
+  });
+});
 
 newgameBtn.addEventListener("click", () => {
-	createApp.style.display = "flex"
-})
+  createApp.style.display = "flex";
+});
